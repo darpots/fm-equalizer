1697c2d6732ac130f206802adb25605da6674de6
Darren Potter <darpots@outlook.com>
2022-04-12 12:50:42 +0100

    first commit

---
+++ src/lib/buttons.svelte
@@ -0,0 +1,53 @@
+<script>
+	export let icon = '';
+	export let text = '';
+	export let type = 'primary';
+</script>
+
+<button class={type}>
+	<img src={icon} alt="" />
+	{text}
+</button>
+
+<style>
+	button {
+		font-weight: 700;
+		font-size: 18px;
+		padding-block: 15px;
+		border-radius: 12px;
+		line-height: 32px;
+		text-align: center;
+		letter-spacing: -0.18px;
+		border: 0;
+		transition: all 0.2s ease-in-out;
+		/* width: 100%; */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.primary {
+		background-color: var(--dark);
+		color: var(--light);
+	}
+
+	.primary:hover {
+		background-color: var(--aqua);
+		color: var(--light);
+	}
+
+	.secondary {
+		background-color: var(--light);
+		color: var(--dark);
+	}
+
+	.secondary:hover {
+		background-color: var(--lightOrange);
+		color: var(--dark);
+	}
+
+	img {
+		outline: 1px solid hotpink;
+		/* margin: -3px 0; */
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '$lib/globalStyles.css';
+
+	import Buttons from '$lib/buttons.svelte';
+</script>
+
+<h1>Connected</h1>
+<Buttons icon="/icon-apple.svg" text="this is my button" />
